a2b5ed355ff33a7ac6dd57e51608f4fb9b768a0b
George English <george.english.ext@boehringer-ingelheim.com>
2021-10-27 08:52:17 -0400

    adding typography types

---
+++ packages/core/typography/types.ts
@@ -0,0 +1,8 @@
+
+export type classes = 'sanserif' | 'serif' | 'slab' | 'script' | 'display' | 'monospace';
+
+export type families = 'Open-Sans' | 'Times-New-Roman' | 'Noto-Serif-Display';
+
+export type styles = 'thin' | 'light' | 'regular' | 'medium' | 'bold' | 'semi-bold' | 'black';
+
+export type placement = 'overline' | 'title' | 'underline' | 'body' | 'label' | 'note' | 'footer';
