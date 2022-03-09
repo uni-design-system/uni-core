78cae682123d0fda959246ffc86d021a5cd949ae
George English <george@pawru.com>
2022-03-09 16:37:36 -0500

    setting up Storybook

---
+++ src/core/theme/themes/light.theme.ts
@@ -0,0 +1,13 @@
+import { ThemeModel } from '../theme.model';
+
+export const LightTheme: ThemeModel = {
+  id: 'LightTheme',
+  name: 'Light Theme',
+  colors: {
+    primary: '#35A7FF',
+    secondary: '#38618C',
+    error: '#FFE74C',
+    warn: '#FF5964',
+    background: '#FFFFFF'
+  }
+}
