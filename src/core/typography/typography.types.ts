374035b444f39d4dcac456de32458a73260adce7
George English <gaenglish@gmail.com>
2022-03-15 17:06:29 -0400

    adding text style types

--- src/core/typography/typography.types.ts
+++ src/core/typography/typography.types.ts
@@ -7,4 +7,22 @@ export type styles = 'thin' | 'light' | 'regular' | 'medium' | 'bold' | 'semi-bo
 
 export type placement = 'overline' | 'title' | 'underline' | 'body' | 'label' | 'note' | 'footer';
 
-export type alignment = 'left' | 'right' | 'center';
+export type TextStyle =
+  | 'headline-1'
+  | 'headline-2'
+  | 'headline-3'
+  | 'headline-4'
+  | 'headline-5'
+  | 'headline-6'
+  | 'body-1-long'
+  | 'body-1-short'
+  | 'body-2-long'
+  | 'body-2-short'
+  | 'title'
+  | 'subtitle'
+  | 'label'
+  | 'caption'
+  | 'overline'
+  | 'paragraph'
+  | 'quote'
+  ;
