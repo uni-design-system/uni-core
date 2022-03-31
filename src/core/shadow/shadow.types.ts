cda9ff510e4f6be514c7a537293cfa4eb870bd22
George English <gaenglish@gmail.com>
2022-03-30 22:28:13 -0400

    adding shadow modes

--- src/core/shadow/shadow.types.ts
+++ src/core/shadow/shadow.types.ts
@@ -1,4 +1,11 @@
 // Shadows styles are listed in order of elevation, low to high.
+
+export type ShadowMode =
+  | 'flat' // Used to turn shadows off
+  | 'interactive'
+  | 'static'
+  ;
+
 export type ShadowElevation =
   | 'pressed'
   | 'raised'
