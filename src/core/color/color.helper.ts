b0b24fdac90054b1439dd913384d1a66307db764
George English <gaenglish@gmail.com>
2022-11-01 06:35:23 -0400

    updating hsla string output

--- src/core/color/color.helper.ts
+++ src/core/color/color.helper.ts
@@ -2,8 +2,8 @@ import { HSLA, UniColor } from './color.model';
 import { CategoryLightness, CategorySaturation, RoleHues } from './color.types';
 import { randomRangeValue } from './color.utils';
 
-export function hslaToString({ hue, saturation, lightness, alpha }: HSLA): string {
-  return `hsla(${hue}, ${saturation}, ${lightness}, ${alpha})`;
+export function hslaToString({ hue, saturation, lightness, alpha = 1 }: HSLA): string {
+  return `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
 }
 
 export function uniColor({ role, category, alpha = 1 }: UniColor): string {
