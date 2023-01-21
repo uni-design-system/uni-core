8b0b1a827a4583426227d3672dde7a9d55f6de7c
George English <gaenglish@gmail.com>
2023-01-21 03:33:09 -0500

    adding SquareTheme

--- src/core/theme/theme.records.ts
+++ src/core/theme/theme.records.ts
@@ -1,11 +1,13 @@
 import { Theme } from './theme.model';
 import { DarkTheme } from './themes/dark.theme';
 import { LightTheme } from './themes/light.theme';
+import { SquareTheme } from './themes/square.theme';
 
 export const BuiltInThemes: Record<string, Theme> = {
   LightTheme,
-  DarkTheme
-}
+  DarkTheme,
+  SquareTheme,
+};
 
 // First Theme is default.
 export const DefaultThemeId = Object.keys(BuiltInThemes)[0];
